eee6f6164cb84798770568d30a5d7d0c993ef811
mohamadhoseinn <amirmm5560@gmail.com>
2026-02-19 23:53:24 +0330

    create table for users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import userRoleEnum from '../enums/userRoleEnum';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  mobile: string;
+
+  @Column({ nullable: false })
+  display_name: string;
+
+  @Column({ nullable: true })
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: userRoleEnum,
+    default: userRoleEnum.NoramlUser,
+  })
+  role: userRoleEnum;
+
+  @Column()
+  creaditAt: Date;
+
+  @Column()
+  updateAt: Date;
+}
